9a56480dd476a2e5b89a52e1fc34ada142dbc6f5
s-blais <stephen@s-blais.com>
2020-04-06 11:02:51 -0400

    Add routes for user & authentication paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#home' # create WelcomeController & welcome/home view
+
+  get '/login' => 'sessions#new' # this will load login form, link to create new user, and eventually omniauth option
+
+  post '/login' => 'sessions#create' # this will establish the user session, and eventually will include if statement to find_or_create omniauth
+
+  # get '/homepage' => 'users#homepage' # this is the user's homepage, initially a list of patients. No, wait, I think this will be covered by user_show_path (show)
+
+  get '/logout' => 'sessions#destroy' # this will be converted to a proper post route with a form
+
+  resource :users #new, create, show, edit, update, delete, index
+
 end
