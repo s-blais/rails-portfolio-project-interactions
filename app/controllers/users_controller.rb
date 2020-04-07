7ad8c2707c07f3a51ce670b0e9bb35240f5c68d5
s-blais <stephen@s-blais.com>
2020-04-07 14:02:03 -0400

    Add index method and fix create method redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+
+  end
+  
   def new
     @user = User.new
   end
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path(@user)
+      redirect_to user_path(@user)
     else
       render :new
     end
