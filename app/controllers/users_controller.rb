5fe1058daa652085eee200b707e71b4bba3c60b5
s-blais <stephen@s-blais.com>
2020-04-07 13:51:10 -0400

    Add minimal user creation functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @user = User.find_by_id(params[:id])
   end
 
   def edit
