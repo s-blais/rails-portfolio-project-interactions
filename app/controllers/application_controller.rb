c6c2abc912ad6b05c5e70ebd9f345fb8f9eb54b0
s-blais <stephen@s-blais.com>
2020-04-09 12:03:15 -0400

    Add login status & current user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+  include ApplicationHelper # or skip this and call these helper methods within the controllers using 'helpers.method_name' (Rails 5+, seems safer?)
+  
+  # helper_method :logged_in?
+  # this would expose method to views if not defined in app/helpers
+  
+  # def logged_in?
+  #   !!session[:user_id]
+  # end
+  
+  # helper_method :require_login
+  # this would expose method to views if not defined in app/helpers
+  
+  # def require_login
+  #   redirect_to root_path unless logged_in?
+  # end
+
+  # helper_method :current_user
+  # this would expose method to views if not defined in app/helpers
+
+  # def current_user
+  #   User.find(session[:used_id])
+  # end
+
+  # database-friendly current_user something more like 
+  # @current_user ||= User.find(session[:user_id])
+  # or @current_user ||= User.find_by_id@(session[:user_id])
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  
+  # def current_user
+  #   User.find(session[:user_id])
+  # end
+
+  # database-friendly current_user is more like 
+  # @current_user ||= User.find(session[:user_id]) or 
+  # @current_user ||= User.find_by_id(session[:user_id])
+
 end
