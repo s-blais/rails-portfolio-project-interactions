bec8a9521c1c6fec308a2030fe2bda9bbba651f3
s-blais <stephen@s-blais.com>
2020-04-09 13:51:45 -0400

    improve post-user-creation redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
+    redirect_back(fallback_location: root_path) if logged_in?
   end
 
   def create
     @user = User.find_by(username: params[:user][:username])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    render 'welcome/home' # ?
+    # render 'welcome/home'
+    redirect_to root_path
   end
 
   def destroy
