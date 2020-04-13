972c99023b647e66da3ceb9bbbcf0828b19aa220
s-blais <stephen@s-blais.com>
2020-04-13 17:22:10 -0400

    Add age calculator to Patient class

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,10 @@
 class Patient < ApplicationRecord
   belongs_to :user
+
+  def age
+    now = Time.now.utc.to_date
+    dob = self.birthdate
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
 end
