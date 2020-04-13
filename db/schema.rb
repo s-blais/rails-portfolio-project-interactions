f3522093e00d8b7aac9d16e4347bebd2faa0e5ae
s-blais <stephen@s-blais.com>
2020-04-13 13:58:26 -0400

    Generate Patient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_175523) do
+ActiveRecord::Schema.define(version: 2020_04_13_175512) do
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "omniauth_uid"
