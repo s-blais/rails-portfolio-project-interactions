b0ea97dfbe7afd9331d0c45cc2fbf046158072db
s-blais <stephen@s-blais.com>
2020-04-17 11:11:15 -0400

    add #my_patient? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
+
+  def my_patient?(patient)
+    current_user == patient.user
+  end
   
   # def current_user
   #   User.find(session[:user_id])
