16c9511b2fc79063ab443f6b88d2bf08bb481cae
s-blais <stephen@s-blais.com>
2020-04-26 16:33:49 -0400

    add .all_alpha for collection_select use

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,4 +1,10 @@
 class Medication < ApplicationRecord
   has_many :prescriptions
   has_many :patients, through: :prescriptions
+
+  def self.all_alpha
+    all.sort_by {|m| m[:name]}
+  end
+
+
 end
