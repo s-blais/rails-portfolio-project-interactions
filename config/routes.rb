d882b2fcc0385bf800f592c3907775f036e1414e
s-blais <stephen@s-blais.com>
2020-04-27 18:45:52 -0400

    clean up comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   
-  root 'welcome#home' # create WelcomeController & welcome/home view
+  root 'welcome#home'
 
-  get '/login' => 'sessions#new' # this will load login form, link to create new user, and eventually omniauth option
+  get '/login' => 'sessions#new'
 
-  post '/login' => 'sessions#create' # this will establish the user session, and eventually will include if statement to find_or_create omniauth
-
-  # get '/homepage' => 'users#homepage' # this is the user's homepage, initially a list of patients. No, wait, I think this will be covered by user_show_path (show)
+  post '/login' => 'sessions#create'
 
   get '/logout' => 'sessions#destroy' # this will be converted to a proper post route with a form
 
   get '/auth/google_oauth2/callback' => 'sessions#create'
 
-  resources :users do
+  resources :users do #maybe add path: 'providers'
     resources :patients, only: [:index, :new, :edit, :update]
   end
 
