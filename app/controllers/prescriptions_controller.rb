8995caee982054ef81fe3adc8def67763e7f8767
s-blais <stephen@s-blais.com>
2020-05-08 11:35:40 -0400

    DRY up with before_action :set_prescription

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,13 +1,13 @@
 class PrescriptionsController < ApplicationController
 
   before_action :require_login
+  before_action :set_prescription, only: [:edit, :update, :destroy]
 
   def index
     @medications = Prescription.all_unique_by_medication
     @prescriptions_count = Prescription.all_tally_by_medication
   end
 
-  # def new (nested only)
   def new
     @patient = Patient.find_by_id(params[:patient_id])
     @prescription = Prescription.new(patient_id: params[:patient_id])
@@ -27,12 +27,10 @@ class PrescriptionsController < ApplicationController
   # there's no show because it's provided in patients#show
 
   def edit
-    @prescription = Prescription.find_by_id(params[:id])
     redirect_to patient_path(params[:patient_id]) unless my_patient?(@prescription.patient)
   end
 
   def update
-    @prescription = Prescription.find_by_id(params[:id])
     if my_patient?(@prescription.patient)
       if @prescription.update(prescription_params)
         redirect_to patient_path(@prescription.patient)
@@ -45,7 +43,6 @@ class PrescriptionsController < ApplicationController
   end
 
   def destroy
-    @prescription = Prescription.find_by_id(params[:id])
     @prescription.destroy if my_patient?(@prescription.patient)
     redirect_to patient_path(@prescription.patient)
   end
@@ -54,6 +51,10 @@ class PrescriptionsController < ApplicationController
 
   def prescription_params
     params.require(:prescription).permit(:patient_id, :medication_id, :dose, :frequency)
+  end
+
+  def set_prescription
+    @prescription = Prescription.find_by_id(params[:id])
   end
 
 
